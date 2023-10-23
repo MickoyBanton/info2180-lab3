0f8b09281560ff4e6e239b1974e196b04a7a8398
Mickoy Banton <mickoybanton11@gmail.com>
2023-10-22 21:06:27 -0500

    Exercise 2

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,9 +1,39 @@
 window.addEventListener('load', function ()
 {
+    let board = document.getElementById("board");
     let gameBoardDiv = board.querySelectorAll("div");
-    
+    const tiles = Array.from(board.getElementsByTagName("div"));
+    let gird=["None","None","None","None","None","None","None","None","None"];
+    let play="X";
+
     gameBoardDiv.forEach((square) =>
     {
         square.classList.add("square"); // Exercise 1
+        square.addEventListener("click",function()
+        {
+            if(gird[tiles.indexOf(square)] =="None" )
+            {
+                
+                if(play == "X")
+                {
+                   gird[tiles.indexOf(square)]= play;
+                   square.textContent= play;
+                   play= "O";
+                   square.classList.add('square', 'X');
+                 }
+                 
+                else if (play == "O")
+                 { 
+                   gird[tiles.indexOf(square)]= play
+                   square.textContent= play;
+                   play= "X";
+                   square.classList.add('square', 'O');
+                 
+                 }  
+                 
+            }
+               
+
+        });
     });
 });
